c25ca3c424f6ced3db61acb0be59900a69316249
hukar <k.meshoub@gmail.com>
2023-05-08 09:48:28 +0200

    i18ntext works

--- LocalisationGlobalizationI18nText/Program.cs
+++ LocalisationGlobalizationI18nText/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using ValidationLocalisation;
+using LocalisationGlobalizationI18nText;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddI18nText();
+
 
 await builder.Build().RunAsync();
